e7c35b10febaae9a011101b24e432bc7048afe48
Steven Yu <stevenyuser@gmail.com>
2022-04-03 18:46:49 -0400

    task model completed

---
+++ SchoolVerseTesting/Models/Task.swift
@@ -0,0 +1,36 @@
+//
+//  Task.swift
+//  SchoolVerseTesting
+//
+//  Created by Steven Yu on 4/3/22.
+//
+
+import Foundation
+import Firebase
+import FirebaseFirestoreSwift
+
+struct Task: Codable, Identifiable {
+    @DocumentID var id: String?
+    var name: String
+    var completed: Bool
+    var dueDate: Date
+    var description: String
+    
+    var courseId: String
+    var courseName: String
+    
+    var userId: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case completed
+        case dueDate = "due_date"
+        case description
+        
+        case courseId = "course_id"
+        case courseName = "course_name"
+        
+        case userId = "user_id"
+    }
+}
--- SchoolVerseTesting/SchoolVerseTestingApp.swift
+++ SchoolVerseTesting/SchoolVerseTestingApp.swift
@@ -6,12 +6,24 @@
 //
 
 import SwiftUI
+import UIKit
+import Firebase
+
+// hooks into application lifecycle
+// source: https://peterfriese.dev/posts/swiftui-new-app-lifecycle-firebase/
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
 struct SchoolVerseTestingApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView() 
+            ContentView()
         }
     }
 }
