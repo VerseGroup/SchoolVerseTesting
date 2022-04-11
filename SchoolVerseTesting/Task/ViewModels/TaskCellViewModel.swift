ef20ab8613547fc0075019a2d0f5f385a55ccf5f
Steven Yu <stevenyuser@gmail.com>
2022-04-11 11:41:25 -0400

    changed task debounce to 10s

--- SchoolVerseTesting/Task/ViewModels/TaskCellViewModel.swift
+++ SchoolVerseTesting/Task/ViewModels/TaskCellViewModel.swift
@@ -38,7 +38,7 @@ class TaskCellViewModel: ObservableObject, Identifiable {
         // updates task when event happens
         $task
             .dropFirst()
-            .debounce(for: 5, scheduler: RunLoop.main) // ensures changes aren't published immediately, have to wait 5 seconds
+            .debounce(for: 10, scheduler: RunLoop.main) // ensures changes aren't published immediately, have to wait 10 seconds
             .sink { task in
                 self.updateTask()
             }
