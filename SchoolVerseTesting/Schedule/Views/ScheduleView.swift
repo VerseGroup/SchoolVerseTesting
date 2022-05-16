162060fe27d48bff19f9f7fe7c2d2a1653aa83e3
Steven Yu <stevenyuser@gmail.com>
2022-05-15 23:24:29 -0400

    added empty/no schedule view

--- SchoolVerseTesting/Schedule/Views/ScheduleView.swift
+++ SchoolVerseTesting/Schedule/Views/ScheduleView.swift
@@ -13,65 +13,64 @@ struct ScheduleView: View {
         VStack {
             header
             VStack {
-                // if schedule valid
-                //            if let schedule = vm.schedule {
-                
-                HStack {
-                    // go to previous day
-                    Button {
-                        vm.updateSelectedDayEvent(date: Calendar.current.date(byAdding: .day, value: -1, to: vm.selectedDate) ?? Date())
-                    } label: {
-                        Image(systemName: "chevron.left")
-                    }
-                    
-                    Spacer()
-                    
-                    Text("\(vm.selectedDayEvent?.day.description ?? "No School!")")
-                        .bold()
-                    
-                    Spacer()
-                    
-                    // go to next day
-                    Button {
-                        vm.updateSelectedDayEvent(date: Calendar.current.date(byAdding: .day, value: 1, to: vm.selectedDate) ?? Date())
-                    } label: {
-                        Image(systemName: "chevron.right")
-                    }
-                }
-                .font(.title2)
-                .padding(.vertical, 2)
-                
-                // main view
-                ScrollView {
-                    VStack(alignment: .leading) {
-                        if let day = vm.selectedDayEvent?.day.description {
-                            Text(day)
+                // if user has a schedule
+                if let _ = vm.schedule {
+                    HStack {
+                        // go to previous day
+                        Button {
+                            vm.updateSelectedDayEvent(date: Calendar.current.date(byAdding: .day, value: -1, to: vm.selectedDate) ?? Date())
+                        } label: {
+                            Image(systemName: "chevron.left")
                         }
-                        Text("\(Date.weekDateString(vm.selectedDate)())")
-                    }
-                    .padding()
-                    .foregroundColor(Color.white)
-                    .background(Color.purple)
-                    .cornerRadius(5)
-                    
-                    // day schedule portion
-                    if let daySchedule = vm.schedule?.days.first(where: { daySchedule in
-                        vm.selectedDayEvent?.day == daySchedule.day
-                    }) {
-                        // ensures ForEach works since PeriodInfo is not unique, but ForEach requires unique
-                        ForEach(
-                            // converting PeriodInfo to UniquePeriodInfo
-                            daySchedule.periods.compactMap({ period in
-                                UniquePeriodInfo(period: period)
-                            })
-                        ) { uniquePeriod in
-                            PeriodView(period: uniquePeriod.period)
+                        
+                        Spacer()
+                        
+                        Text("\(vm.selectedDayEvent?.day.description ?? "No School!")")
+                            .bold()
+                        
+                        Spacer()
+                        
+                        // go to next day
+                        Button {
+                            vm.updateSelectedDayEvent(date: Calendar.current.date(byAdding: .day, value: 1, to: vm.selectedDate) ?? Date())
+                        } label: {
+                            Image(systemName: "chevron.right")
                         }
                     }
+                    .font(.title2)
+                    .padding(.vertical, 2)
+                    
+                    // main view
+                    ScrollView {
+                        VStack(alignment: .leading) {
+                            if let day = vm.selectedDayEvent?.day.description {
+                                Text(day)
+                            }
+                            Text("\(Date.weekDateString(vm.selectedDate)())")
+                        }
+                        .padding()
+                        .foregroundColor(Color.white)
+                        .background(Color.purple)
+                        .cornerRadius(5)
+                        
+                        // day schedule portion
+                        if let daySchedule = vm.schedule?.days.first(where: { daySchedule in
+                            vm.selectedDayEvent?.day == daySchedule.day
+                        }) {
+                            // ensures ForEach works since PeriodInfo is not unique, but ForEach requires unique
+                            ForEach(
+                                // converting PeriodInfo to UniquePeriodInfo
+                                daySchedule.periods.compactMap({ period in
+                                    UniquePeriodInfo(period: period)
+                                })
+                            ) { uniquePeriod in
+                                PeriodView(period: uniquePeriod.period)
+                            }
+                        }
+                    }
+                } else {
+                    Text("Connect your schedule!")
                 }
-                //            } else {
-                //                Text("Connect your schedule!")
-                //            }
             }
         }
         .padding()
